6d6a43c27cb56a28927399a1d5e127357d23f911
Cade Weinberg <cadeweinberg@gmail.com>
2020-01-05 20:38:54 -0800

    smol msg

--- pink/typeset.h
+++ pink/typeset.h
@@ -15,6 +15,7 @@ using std::unordered_map;
 // and when we want to test for set membership we get that value
 // and look it up in the symset, which is a bit-compact,
 // random-access lookup, based on the string value, that can dynamically grow.
+// the speed is payed for by
 
 // naive implementation.
 class Symset {
@@ -22,18 +23,18 @@ class Symset {
 	Symenum  symenum;
 
 public:
-	int get_sym_num(Type& type) {
+	int get_sym_num(string& str) {
 		return symenum.lookup(type);
 	}
 
-	void insert(Type& type) {
+	void insert(string& str) {
 		auto symnum = get_sym_num(type);
 		if (set.size() < symnum)
 			set.insert((set.begin() + symnum), true);
 		else set[symnum] = true;
 	}
 
-	bool lookup(Type& type) {
+	bool lookup(string& str) {
 		auto symnum = get_sym_num(type);
 		if (set.size() < symnum) {
 			set.insert((set.begin() + symnum), false);
@@ -51,12 +52,12 @@ class Symenum {
 	unordered_map<string, int> map;
 
 public:
-	void insert(Type& type) {
-		map[type.getname()] = numsyms;
+	void insert(string& str) {
+		map[str.c_str()] = numsyms;
 		numsyms++;
 	}
 	// 0 is the implicit error case;
-	int lookup(Type& type) {
-		return map[type.getname()];
+	int lookup(string& str) {
+		return map[str.c_str()];
 	}
 };
